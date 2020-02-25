3c9f690d328ec292914b2f497eebdc9b1f834c0e
vagrant <vagrant@localhost.localdomain>
2020-02-26 07:45:42 +0900

    userモデルの作成
    データベースが必要だから

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200222175036) do
+ActiveRecord::Schema.define(version: 20200225203348) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
