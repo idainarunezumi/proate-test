5ba74c4999ce225df39499d2eae2d23e85ddcc0a
vagrant <vagrant@localhost.localdomain>
2020-02-26 05:20:32 +0900

    posts/newでエラーメッセージが出るようにした
    エラーになったのかなっていないのか分からないから

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(content:params[:content])
