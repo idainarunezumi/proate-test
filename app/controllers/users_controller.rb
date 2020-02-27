56ae4ed3012b07fe61f47dde6882dd83612b1692
vagrant <vagrant@localhost.localdomain>
2020-02-27 13:26:44 +0900

    posts/indexのユーザー画像がきれいに表示されるようにCSS、HTMlを編集
    画像がうまく表示されていなかったから

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name: "nigaoe_sakaguchi_ango.png"
+      image_name: "yellow.png"
     )
     if @user.save
       flash[:notice] = "新規登録が完了しました"
