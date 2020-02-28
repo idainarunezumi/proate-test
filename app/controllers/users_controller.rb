a56b03877aff56f6faf012329128b247f169f28d
vagrant <vagrant@localhost.localdomain>
2020-02-28 15:19:39 +0900

    sessionの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       flash[:notice] = "ログインしました"
+      session[:user.id] = @user.id
       redirect_to("/users/index")
     else
       @error_message = "emailまたはパスワードが間違っています"
